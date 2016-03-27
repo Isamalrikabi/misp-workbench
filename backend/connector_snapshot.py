f39182ec6ca7751fd4fdffe17a40fae954c98908
Raphaël Vinot <raphael@vinot.info>
2016-03-27 19:32:25 +0000

    Add comments

--- backend/connector_snapshot.py
+++ backend/connector_snapshot.py
@@ -15,40 +15,55 @@ class SnapshotConnector(object):
 
     def make_hashed_value(self, value):
         '''
-            Hash the value to search in the database
+            Hash the value to search
         '''
         return SHA256.new(value.strip().lower()).hexdigest()
 
     def get_value_details(self, hashed_value):
+        '''
+            Returns all attributes of a value
+        '''
         attributes_ids = self.r.smembers('{}:attrs'.format(hashed_value))
         return [self.r.hgetall('attribute:{}'.format(attrid)) for attrid in attributes_ids]
 
     def get_value_digest(self, hashed_value):
+        '''
+            Returns value1 & 2 and comment, deduplicate
+        '''
         attrids = self.r.smembers('{}:attrs'.format(hashed_value))
         digest = [self.r.hmget('attribute:{}'.format(aid), 'value1', 'value2', 'comment') for aid in attrids]
         values = set()
         comments = set()
         for v1, v2, comment in digest:
-            if comment:
-                comments.add(comment)
             values.add(v1)
             if v2:
                 values.add(v2)
+            if comment:
+                comments.add(comment)
         return values, comments
 
-    def get_events_digest(self, hashed_value):
+    def get_events_digest_from_value(self, hashed_value):
+        '''
+            Returns digests of events the value is listed in.
+        '''
         eids = self.r.smembers(hashed_value)
         infos = sorted([self.get_event_digest(eid) for eid in eids], key=lambda tup: int(tup[0]))
         return infos
 
-    # ##### Similarities #####
+    # ##### Keys functions #####
 
-    def intersection_details(self, key):
+    def key_values_digests(self, key):
+        '''
+            Returns value digests of all values in a key
+        '''
         return [self.get_value_digest(hashed_value) for hashed_value in self.r.smembers(key)]
 
     # ##### Event functions #####
 
     def get_event_digest(self, eid):
+        '''
+            Returns info and date of the event
+        '''
         return eid, self.r.hmget('event:{}'.format(eid), 'info', 'date'), self.r.smembers('event:{}:tags'.format(eid))
 
     def merge(self, events):
@@ -76,11 +91,17 @@ class SnapshotConnector(object):
         return out_key
 
     def events_similarities(self, *events):
+        '''
+            Returns the intersection and the total amount of values in multiple events
+        '''
         return self.r.scard(self.intersection(events)), self.r.scard(self.merge(events))
 
     # ##### Group functions #####
 
     def make_group(self, name, *events):
+        '''
+            Create a group of events
+        '''
         if not self.r.exists(name):
             self.r.sadd(name, *events)
             self.r.sadd('groups', name)
@@ -88,14 +109,24 @@ class SnapshotConnector(object):
             raise Exception('Group name already exists, maybe you want to update.')
 
     def update_group(self, name, *events):
+        '''
+            Update a group of events
+        '''
         self.r.sadd(name, *events)
         self.r.sadd('groups', name)
 
     def del_group(self, name):
+        '''
+            Delete a group of events
+        '''
         self.r.delete(name)
         self.r.srem('groups', name)
 
     def merge_groups(self, group_names):
+        '''
+            Merge groups of events
+            The key of the set is <group1>|<group2>|<group3>|...
+        '''
         groups = sorted([self.merge(self.r.smembers(group_name)) for group_name in group_names])
         out_key = '|'.join(groups)
         self.r.sunionstore(out_key, *groups)
@@ -103,6 +134,11 @@ class SnapshotConnector(object):
         return out_key
 
     def intersection_groups(self, group_names):
+        '''
+            Keeps only the values in *all* the sets
+            The key of the set is <group1>&<group2>&<group3>&...
+        '''
+
         groups = sorted([self.intersection(self.r.smembers(group_name)) for group_name in group_names])
         out_key = '&'.join(groups)
         self.r.sinterstore(out_key, *groups)
@@ -110,4 +146,7 @@ class SnapshotConnector(object):
         return out_key
 
     def groups_similarities(self, *group_names):
+        '''
+             Returns the intersection and the total amount of values in multiple groups
+        '''
         return self.r.scard(self.intersection_groups(group_names)), self.r.scard(self.merge_groups(group_names))
